b403c87cfde21f78664a4d9e7aa3f9c82eabdc68
Studio-OLabs <104942153+Studio-OLabs@users.noreply.github.com>
2022-05-12 20:19:48 -0300

    Add files via upload

---
+++ MoveCharacterTouchScreen.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacterTouchScreen : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    //private GameController gameController;
+
+    private void Start()
+    {
+       // gameController = FindObjectOfType<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DragTouch();
+    }
+
+    private void DragTouch()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            transform.Translate(touchDeltaPosition.x * speed * Time.deltaTime, 0f, 0f);
+        }
+    }
+}
